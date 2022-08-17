61fcb532819e9a985951b218ef47980223d07d93
schenkenberger.ch@gmail.com <wcente007>
2022-08-17 22:59:17 +0200

    Init

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,7 +21,7 @@ public class App extends ListenerAdapter {
 
     public App() throws LoginException, InterruptedException {
 
-        jda = JDABuilder.createDefault(BibschUtils.prop("token"))
+        jda = JDABuilder.createDefault(BibschUtils.prop("tokens"))
                 .build();
         jda.addEventListener(new MusicListener());
         jda.addEventListener(new MemeListener());
